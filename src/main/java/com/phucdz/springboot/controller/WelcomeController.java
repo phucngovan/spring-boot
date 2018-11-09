f511e7501c693f6484fdf7d4ceb7e5885f6293c0
phucngovan <phucdepzai0712@gmail.com>
2018-11-09 14:31:39 +0700

    Initial commit

---
+++ src/main/java/com/phucdz/springboot/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.phucdz.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class WelcomeController {
+    // inject via application.properties
+    @Value("Welcome!")
+    private String message = "Welcome!";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+}
